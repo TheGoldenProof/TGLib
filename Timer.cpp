36c5014dfb5fb0149f6c4d3a30b9b0c1b5ca9729
TheGoldenProof <firebirdspiral@gmail.com>
2023-09-28 20:13:39 +0000

    Update timer

--- Timer.cpp
+++ Timer.cpp
@@ -11,24 +11,6 @@ Timer::Timer() {
 	pauseTime = nanoseconds::zero();
 }
 
-f32 Timer::Mark() noexcept {
-	const auto old = last;
-	last = steady_clock::now();
-	totalTime = last - old;
-	const auto activeTime = totalTime - pauseTime;
-	pauseLast = last;
-	pauseTime = nanoseconds::zero();
-	return duration<f32>(activeTime).count();
-}
-
-f32 Timer::Peek() const noexcept {
-	const auto curr = steady_clock::now();
-	const auto totalTime_ = curr - last;
-	const auto pauseTime_ = pauseTime + (paused? (curr - pauseLast) : nanoseconds::zero());
-	const auto activeTime = totalTime_ - pauseTime_;
-	return duration<f32>(activeTime).count();
-}
-
 void Timer::SetPaused(bool paused_) noexcept {
 	if (paused_ == paused) return;
 	paused = paused_;
--- Timer.h
+++ Timer.h
@@ -7,8 +7,37 @@ namespace TGLib {
 class Timer {
 public:
 	Timer();
-	f32 Mark() noexcept; // does not unpause
-	f32 Peek() const noexcept;
+
+	template <typename Rep = f32, typename Period = std::ratio<1>>
+	std::chrono::duration<Rep, Period> Mark_dur() noexcept {
+		using namespace std::chrono;
+		const auto old = last;
+		last = steady_clock::now();
+		totalTime = last - old;
+		const auto activeTime = totalTime - pauseTime;
+		pauseLast = last;
+		pauseTime = steady_clock::duration::zero();
+		return duration_cast<duration<Rep, Period>>(activeTime);
+	}
+	template <typename Rep = f32, typename Period = std::ratio<1>>
+	Rep Mark() noexcept {
+		return Mark_dur<Rep, Period>().count();
+	}
+
+	template <typename Rep = f32, typename Period = std::ratio<1>>
+	std::chrono::duration<Rep, Period> Peek_dur() const noexcept {
+		using namespace std::chrono;
+		const auto curr = steady_clock::now();
+		const auto totalTime_ = curr - last;
+		const auto pauseTime_ = pauseTime + (paused ? (curr - pauseLast) : steady_clock::duration::zero());
+		const auto activeTime = totalTime_ - pauseTime_;
+		return duration_cast<duration<Rep, Period>>(activeTime);
+	}
+	template <typename Rep = f32, typename Period = std::ratio<1>>
+	Rep Peek() const noexcept {
+		return Peek_dur<Rep, Period>().count();
+	}
+
 	void SetPaused(bool paused) noexcept;
 	bool IsPaused() const noexcept;
 private:
